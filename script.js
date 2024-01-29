ec853aa606296af4fb2aa3087781ed2cd1ffd733
rouman1201082 <chernovrn@gmail.com>
2024-01-29 18:23:58 +0400

    29.01.24

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+"use strict";
+
 const nameProduct = 'apple';
-const quantityProduct = 'quantity';
-const categoryProduct = 'category';
-const priceProduct = 'price';
+const quantityProduct = 20;
+const categoryProduct = 'fruit';
+const priceProduct = 10;
 
 console.log(nameProduct);
 
